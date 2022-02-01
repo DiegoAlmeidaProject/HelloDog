a5864b9706ffa5184e9ace77f9a6a4f817039c40
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-01-31 21:19:09 -0300

    Implementando o Style: TextStyle nop Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.blue
-      ),
+      theme: ThemeData(primaryColor: Colors.blue),
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello Flutter"),
+        title: Text(
+          "Hello Flutter",
+        ),
         centerTitle: true,
       ),
       body: Container(
         color: Colors.white,
+        child: Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.red,
+              decorationStyle: TextDecorationStyle.wavy,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
-
